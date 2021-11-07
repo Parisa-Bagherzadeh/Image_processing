a631f0fda53c10d702c83aba676f2f8019aad199
Parisa-Bagherzadeh <71878421+Parisa-Bagherzadeh@users.noreply.github.com>
2021-11-07 23:58:32 +0330

    Delete noise,merge directory

--- noise,merge/code/board.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-test=cv2.imread('test.bmp',0)
-origin=cv2.imread('origin.bmp',0)
-
-result=cv2.subtract(test,origin)
-
-cv2.imwrite('board.jpg',result)
--- noise,merge/code/ex22_1.py
+++
@@ -1,8 +0,0 @@
-import cv2
-img1=cv2.imread('a.tif',0)
-img2=cv2.imread('b.tif',0)
-
-img3=img2-img1
-cv2.imwrite('result2.jpg',img3)
-cv2.imshow('image',img3)
-cv2.waitKey()

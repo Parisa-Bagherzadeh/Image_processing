542d12c67e69993f62983d282f45c937ef28208a
Parisa-Bagherzadeh <71878421+Parisa-Bagherzadeh@users.noreply.github.com>
2021-12-09 21:01:31 +0330

    Add files via upload

---
+++ Assignment26-2/mnsit.py
@@ -0,0 +1,26 @@
+import cv2
+import os 
+img=cv2.imread('mnist.png',0)
+rows=img.shape[0]
+cols=img.shape[1]
+height=rows//(10*5)
+width=cols//100
+counter=0
+folder_counter=-1
+
+for i in range(10):
+    os.makedirs(f'mnist_images/{i}')
+
+for k in range(0,rows,height*5):
+    folder_counter+=1
+    for i in range(k,k+(height*5),height):
+        for j in range(0,cols,width):
+            small_img=img[i:i+height,j:j+width]
+            cv2.imwrite(f'mnist_images/{folder_counter}/{counter}.jpg',small_img)
+            counter+=1
+            
+
+
+
+
+
